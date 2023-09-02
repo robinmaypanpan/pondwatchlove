866fa940aca32614c697221f6947dbfaa7a60933
Robin <robinmaypanpan@gmail.com>
2023-09-02 18:44:12 -0400

    Support Int layers without tiles

--- lib/ldtk/TileLayer.lua
+++ lib/ldtk/TileLayer.lua
@@ -26,41 +26,45 @@ function TileLayer:initialize(data, builder, level)
         tiles = data.gridTiles
     end
 
-    for _, tileData in ipairs(tiles) do
-        local tile = {
-            id = tileData.t,
-            sourceLocation = { x = tileData.src[1], y = tileData.src[2] },
-            drawLocation = { x = tileData.px[1], y = tileData.px[2] },
-            flipX = tileData.f == 1 or tileData.f == 3,
-            flipY = tileData.f == 2 or tileData.f == 3,
-            opacity = tileData.a,
-            data = tileData
-        }
+    if tiles ~= nil and #tiles > 0 then
+        for _, tileData in ipairs(tiles) do
+            local tile = {
+                id = tileData.t,
+                sourceLocation = { x = tileData.src[1], y = tileData.src[2] },
+                drawLocation = { x = tileData.px[1], y = tileData.px[2] },
+                flipX = tileData.f == 1 or tileData.f == 3,
+                flipY = tileData.f == 2 or tileData.f == 3,
+                opacity = tileData.a,
+                data = tileData
+            }
 
-        tile.quad = self.tileset:getTileQuad(tile.id)
+            tile.quad = self.tileset:getTileQuad(tile.id)
 
-        table.insert(self.tiles, tile)
+            table.insert(self.tiles, tile)
+        end
+
+        self.tilesetBatch = self.tileset:createSpriteBatch(self.numRows, self.numCols)
     end
-
-    self.tilesetBatch = self.tileset:createSpriteBatch(self.numRows, self.numCols)
 end
 
 -- Draws this current tile layer
 function TileLayer:draw()
-    self.tilesetBatch:clear()
-    for _, tile in ipairs(self.tiles) do
-        local scaleX = 1
-        local scaleY = 1
-        if tile.flipX then
-            scaleX = -1
+    if self.tilesetBatch then
+        self.tilesetBatch:clear()
+        for _, tile in ipairs(self.tiles) do
+            local scaleX = 1
+            local scaleY = 1
+            if tile.flipX then
+                scaleX = -1
+            end
+            if tile.flipY then
+                scaleY = -1
+            end
+            self.tilesetBatch:add(tile.quad, tile.drawLocation.x, tile.drawLocation.y, 0, scaleX, scaleY)
         end
-        if tile.flipY then
-            scaleY = -1
-        end
-        self.tilesetBatch:add(tile.quad, tile.drawLocation.x, tile.drawLocation.y, 0, scaleX, scaleY)
+        self.tilesetBatch:flush()
+        love.graphics.draw(self.tilesetBatch, self.level.x, self.level.y)
     end
-    self.tilesetBatch:flush()
-    love.graphics.draw(self.tilesetBatch, self.level.x, self.level.y)
 end
 
 return TileLayer
--- main.lua
+++ main.lua
@@ -20,7 +20,7 @@ function love.load()
     })
 
     local builder = LevelBuilder:new(entityTable)
-    world = builder:load('assets/levels/test1.ldtk')
+    world = builder:load('assets/levels/test_avery.ldtk')
 
     world:activateLevel('Entrance')
 end
