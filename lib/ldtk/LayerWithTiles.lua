864c4adb4dbeac5e5dd0d3e335346e90b6e0b2a7
Robin <robinmaypanpan@gmail.com>
2023-09-03 20:35:15 -0400

    Added getTileRange command

--- lib/ldtk/LayerWithTiles.lua
+++ lib/ldtk/LayerWithTiles.lua
@@ -20,6 +20,22 @@ function LayerWithTiles:initialize(data, builder, level)
     self.layerDef = builder:getLayerDefinition(self.id)
 end
 
+-- Ask for all the tiles between these two locations
+function LayerWithTiles:getTilesInRange(row1, col1, row2, col2)
+    assert(row1 == row2 or col1 == col2,
+        'getTilesInRange only supports horizontally or vertically aligned lines of tiles at this time')
+    local tiles = {}
+    for row = row1, row2 do
+        for col = col1, col2 do
+            local tile = self:getTile(row, col)
+            if tile then
+                table.insert(tiles, tile)
+            end
+        end
+    end
+    return tiles
+end
+
 -- Returns a given tile
 function LayerWithTiles:getTile(row, col)
     assert(false, 'getTile method not implemented in child class')
@@ -27,16 +43,20 @@ end
 
 -- Returns the tile at the specified x,y level coordinates
 function LayerWithTiles:getTileInLevel(x, y)
-    local row = math.floor(y / self.tileSize)
-    local col = math.floor(x / self.tileSize)
-
-    return self:getTile(row, col)
+    return self:getTile(self:convertLevelToGrid(x, y))
 end
 
 -- Returns the tile at the specified x,y world coordinates
 -- NB: Returns nil outside the level
 function LayerWithTiles:getTileInWorld(x, y)
-    return self:getTileInLevel(x - self.level.x, y - self.level.y)
+    return self:getTileInLevel(self.level:convertWorldToLevel(x, y))
+end
+
+-- Converts level-relative x,y coordinates to row,col coordinates on the local grid
+function LayerWithTiles:convertLevelToGrid(x, y)
+    local row = math.floor(y / self.tileSize)
+    local col = math.floor(x / self.tileSize)
+    return row, col
 end
 
 -- Super function that should be overriden
